bcb0d2cb5546c3eaa47418adfd7f5f6f0aae4122
Srushti Vedpathak <srushtivedpathak@gmail.com>
2026-09-07 04:44:37 +0530

    Add database initialization and update gitignore

--- smartbankdatabase.sql
+++ smartbankdatabase.sql
@@ -18,7 +18,8 @@
 --
 -- Table structure for table `accounts`
 --
-
+CREATE DATABASE IF NOT EXISTS smartbank;
+USE smartbank;
 DROP TABLE IF EXISTS `accounts`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
